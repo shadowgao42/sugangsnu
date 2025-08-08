061acd243f02ace2b132c0ba87d14ae7e07cf1dd
shadowgao42 <152411214+shadowgao42@users.noreply.github.com>
2025-08-08 16:18:10 +0900

    Update sugang.py

--- sugang.py
+++ sugang.py
@@ -163,9 +163,10 @@ def render():
     status = "만석" if current >= quota else "여석 있음"
     pct_display = current / quota * 100 if quota else 0
     st.write(
-        f"**상태:** {status}  |   **현재 학생 비율:** {pct_display:.0f}% <br>" f"**{cls.strip():0>3}분반** {prof}"
-        unsafe_allow_html=True
-    )
+        f"**상태:** {status}  |   **현재 학생 비율:** {pct_display:.0f}% <br>" 
+        f"{cls.strip():0>3}분반 {prof}",
+    unsafe_allow_html=True
+)
 
 
 with placeholder.container():
