2cd3e728e55c014a24f7ae31f6ce5c2b458041bb
shadowgao42 <152411214+shadowgao42@users.noreply.github.com>
2025-08-08 16:41:33 +0900

    Update and rename st.set_page_config,py to st.set_page_config.py

--- st.set_page_config.py
+++ st.set_page_config.py
@@ -3,7 +3,7 @@ from PIL import Image   # only if you want to load a local image file
 
 st.set_page_config(
     page_title="수강신청 모니터",          # 탭 제목
-    page_icon="assets/favicon.ico",       # 로컬 경로 / URL / emoji / emoji-shortcode / Material-icon
+    page_icon="/favicon.ico",       # 로컬 경로 / URL / emoji / emoji-shortcode / Material-icon
     layout="wide",
     initial_sidebar_state="collapsed",
 )
